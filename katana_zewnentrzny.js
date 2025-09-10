d8bc3a5b8f2c57cda34164d9ecc7877fe511fc3d
BonsolPL <szymonzmarsa@tlen.pl>
2025-09-10 16:45:44 +0200

    Add files via upload

---
+++ katana_zewnentrzny.js
@@ -0,0 +1,2 @@
+document.write("<b>" + "ostatnia modyfikacja strony".fontcolor("purple").fontsize(5)+"</b>" + "<br>"); 
+document.write("<b>" + document.lastModified .fontcolor("purple").fontsize(5) + "</b>");
